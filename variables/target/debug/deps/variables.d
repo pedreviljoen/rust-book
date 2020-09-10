c9d0e899eaab196cff078c3cb142c0dd4e5d192d
pedreviljoen <pedre87@gmail.com>
2020-09-10 22:10:28 +0200

    Chapter 3 done

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/pedreviljoen/Documents/Personal/open-source/rust-book/variables/target/debug/deps/variables: src/main.rs
+
+/Users/pedreviljoen/Documents/Personal/open-source/rust-book/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/pedreviljoen/Documents/Personal/open-source/rust-book/variables/target/debug/variables: /Users/pedreviljoen/Documents/Personal/open-source/rust-book/variables/src/main.rs
